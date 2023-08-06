67b44a9176a3c0bedce55a61e1c75b13fe3e33c4
Jocarl Basco <jocarlbasco24@gmail.com>
2023-08-07 02:29:29 +0800

    Dev 03: Communities

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -4,6 +4,7 @@ import * as z from 'zod';
 import { useForm } from 'react-hook-form';
 import { usePathname, useRouter } from 'next/navigation';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { useOrganization } from '@clerk/nextjs';
 
 import {
   Form,
@@ -35,6 +36,7 @@ interface Props {
 const PostThread = ({ userId }: { userId: string }) => {
   const router = useRouter();
   const pathname = usePathname();
+  const { organization } = useOrganization();
 
   const form = useForm<z.infer<typeof ThreadValidation>>({
     resolver: zodResolver(ThreadValidation),
@@ -48,7 +50,7 @@ const PostThread = ({ userId }: { userId: string }) => {
     await createThread({
       text: values.thread,
       author: userId,
-      communityId: null,
+      communityId: organization ? organization.id : null,
       path: pathname,
     });
 
